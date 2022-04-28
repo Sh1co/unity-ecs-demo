7b2e441a9cd365e29ccf09f4950888cc7d4c519b
Sherif Nafee <shico.m.nafee@gmail.com>
2022-04-28 23:14:55 +0300

    Add camera follow

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,9 +1,25 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
-namespace DefaultNamespace
+
+public class CameraFollow : MonoBehaviour
 {
-    public class CameraFollow : MonoBehaviour
+    [SerializeField] private Transform _followTarget;
+
+    private void Awake()
     {
-        
+        Vector3 initialPosition = transform.position;
+        _offset = initialPosition - _followTarget.position;
+        _fixedY = initialPosition.y;
     }
+
+    private void LateUpdate()
+    {
+        Vector3 newPosition = _followTarget.position + _offset;
+        newPosition.y = _fixedY;
+        transform.position = newPosition;
+    }
+
+    private Vector3 _offset;
+    private float _fixedY;
 }
